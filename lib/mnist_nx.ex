89f86d10f01bd39ae02c2a2145902ab6c3a1f891
Yevhen Shemet <yevhene@gmail.com>
2021-02-18 11:37:42 +0200

    Cleanup

--- lib/mnist_nx.ex
+++ lib/mnist_nx.ex
@@ -1,18 +1,2 @@
 defmodule MnistNx do
-  @moduledoc """
-  Documentation for `MnistNx`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MnistNx.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/mnist_nx_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MnistNxTest do
-  use ExUnit.Case
-  doctest MnistNx
-
-  test "greets the world" do
-    assert MnistNx.hello() == :world
-  end
-end
